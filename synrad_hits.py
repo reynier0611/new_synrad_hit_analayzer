c88045ec96743e00839f5dc254bbda5b1f0abeea
Reynier Cruz Torres <reynier0611@gmail.com>
2022-10-18 16:47:56 -0700

    Fixed issues and added an option to create preprocessed geant hits table so that then we can read it without having to reopen root files

--- synrad_hits.py
+++ synrad_hits.py
@@ -7,12 +7,14 @@ import numpy as np
 import uproot
 import matplotlib.pyplot as plt
 import time
+from silx.io.dictdump import dicttoh5, h5todict
+import argparse
 
 # ------------------------------------------------------------------------------------------------
 # Class to analyze synrad hits
 # ------------------------------------------------------------------------------------------------
 class hits_from_synrad:
-    def __init__(self,nevents,int_window=100.e-09,path_to_photons=None,path_to_hits=None):
+    def __init__(self,nevents,int_window=100.e-09,path_to_photons=None,path_to_hits=None,preprocess_g4_hits=True):
         if path_to_photons==None or path_to_hits==None:
             print("need both 'path_to_photons' and 'path_to_hits' arguments")
             exit()
@@ -36,9 +38,6 @@ class hits_from_synrad:
 
         self.df_photons = pd.read_csv(os.path.join(self.path_to_photons,'normalization_file.txt'))
         self.df_photons.columns = ['facet','subidx','NormFact']
-        print(self.df_photons.info())
-        print(self.df_photons.describe())
-        print('')
 
         n_entries = len(self.df_photons)
         self.h1_df = ROOT.TH1D('h1_df',';entry;W [#gamma/sec]',n_entries,0,n_entries)
@@ -58,9 +57,10 @@ class hits_from_synrad:
             for var in ['x','y','z']:
                 self.hits[detector][var] = []
 
-        self.load_hits()
-
-        print('>>>',self.hit_container['25111'][5]['VertexBarrelHits'])
+        if preprocess_g4_hits:
+            self.preprocess_hits()
+        else:
+            self.load_hits()
 
     # --------------------------------------------
     def generate_an_event(self):
@@ -89,11 +89,12 @@ class hits_from_synrad:
     # --------------------------------------------
     def generate(self):
         multiplicity = []
-        print('')
+
+        print('Beginning loop over events')
 
         # Loop over and generate events the user requested
         for i in range(self.nevents):
-            if i%50==0:
+            if i%100==0:
                 print('Event',i,'out of',self.nevents)
 
             event = self.generate_an_event()
@@ -114,18 +115,18 @@ class hits_from_synrad:
             self.plot_x_y_z_distributions(detector)
 
     # --------------------------------------------
-    def load_hits(self):
+    def preprocess_hits(self):
         self.hit_container = {}
         for facet in self.facets:
             self.hit_container[facet] = {}
             for seed in range(1,self.number_seeded_geant_files):
-                self.hit_container[facet][seed] = {}
+                self.hit_container[facet][(str)(seed)] = {}
 
                 fname = os.path.join(self.path_to_hits,'geant_out_{}_seed_{}.edm4hep.root'.format(facet,seed))
                 F = uproot.open(fname)
 
                 for detector in self.detectors:
-                    self.hit_container[facet][seed][detector] = {}
+                    self.hit_container[facet][(str)(seed)][detector] = {}
                     try:
                         subdf_x = F['events/'+detector+'.position.x'].array(library="pd")
                         subdf_y = F['events/'+detector+'.position.y'].array(library="pd")
@@ -139,19 +140,35 @@ class hits_from_synrad:
                                 idxs.append(subdf_x.index[i][0])
 
                         for idx in idxs:
-                            self.hit_container[facet][seed][detector][idx] = []
+                            self.hit_container[facet][(str)(seed)][detector][(str)(idx)] = []
                             for sh in range(len(subdf_x[idx])):
-                                self.hit_container[facet][seed][detector][idx].append([sh,subdf_x[idx][sh],subdf_y[idx][sh],subdf_z[idx][sh]])
+                                self.hit_container[facet][(str)(seed)][detector][(str)(idx)].append([sh,subdf_x[idx][sh],subdf_y[idx][sh],subdf_z[idx][sh]])
                     except:
                         pass
 
+        filename = 'preprocessed_G4_hits_seeds_{}.h5'.format(self.number_seeded_geant_files)
+        print(f'Writing results to {filename}')
+        dicttoh5(self.hit_container,filename, overwrite_data=True)
+
+    # --------------------------------------------
+    def load_hits(self):
+        '''
+        Load preprocessed dictionary with Geant hits including seed information.
+        '''
+        filename = 'preprocessed_G4_hits_seeds_{}.h5'.format(self.number_seeded_geant_files)
+        self.hit_container = h5todict(filename)
+
     # --------------------------------------------
     def locate_hits(self,facet,idx):
+        '''
+        given a facet number and photon index, generate a random integer in [1,number of seeded geant files] and
+        determine if there were hits for the given detectors
+        '''
         seed = np.random.randint(1,self.number_seeded_geant_files)
 
         for detector in self.detectors:
             try:
-                elm = self.hit_container[facet][seed][detector][idx]
+                elm = self.hit_container[facet][(str)(seed)][detector][(str)((int)(idx))]
                 for i in range(len(elm)):
                     self.hits[detector]['x'].append(elm[i][1])
                     self.hits[detector]['y'].append(elm[i][2])
@@ -166,23 +183,38 @@ class hits_from_synrad:
         y = self.hits[detector]['y']
         z = self.hits[detector]['z']
 
-        plt.figure(figsize=(10,5))
+        fig = plt.figure(figsize=(10,5))
         plt.subplot(1,2,1)
         plt.scatter(x,y)
         plt.subplot(1,2,2)
         plt.scatter(z,x)
 
-        plt.savefig('results_xyz_hits_'+detector+'.png',dpi=600)
+        plt.savefig('output_plots/results_xyz_hits_'+detector+'.png',dpi=600)
+        del fig
+        plt.close()
 
 # ------------------------------------------------------------------------------------------------
 # Main function
 # ------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     t0 = time.time()
+
+    parser = argparse.ArgumentParser(description='Analyzing hits from synchrotron radiation')
+    parser.add_argument('--process_g4_hits', 
+                        help='process geant hits and save dictionary to hdf5', 
+                        action='store_true', default=False)
+    parser.add_argument('--analyze', 
+                        help='load hits from dictionary and analyze', 
+                        action='store_true', default=False)
+    args = parser.parse_args()
+
     print('Creating an instance of hits_from_synrad')
     path_to_photons = './'
     path_to_hits = 'geant_data/'
     nevents = 1000
-    hits = hits_from_synrad(nevents,100.e-09,path_to_photons,path_to_hits) # argument is integration window in sec
-    hits.generate()
+    hits = hits_from_synrad(nevents,100.e-09,path_to_photons,path_to_hits,args.process_g4_hits) # argument is integration window in sec
+    if args.analyze:
+        hits.generate()
+
+    print()
     print('Overall running time:',np.round((time.time()-t0)/60.,2),'min')
